96081b2ed5d12c0b3c2029e0d4dced8fc3d660ad
levovix0 <xlevovix@gmail.com>
2022-01-11 23:20:18 +0300

    doc: render; add: render image

--- src/render.nim
+++ src/render.nim
@@ -3,24 +3,23 @@ import pixie
 import syntax_highlighting
 
 type
-  Glyph* = tuple[width, height: int32, data: seq[ColorRgbx]]
-
   GlyphTable* = object
+    ## table of pre-renered text images
     font*: Font
-    glyphs: Table[tuple[c: Rune; fg, bg: ColorRgb], Glyph]
+    glyphs: Table[tuple[c: Rune; fg, bg: ColorRgb], Image]
 
 
 {.push, checks: off.}
 
-func drawGlyph(r: Image, g: Glyph, pos: IVec2, size: IVec2) =
-  ## fast draw image
+func draw*(r: Image, g: Image, pos: IVec2, size: IVec2) =
+  ## draw image (fast)
 
   # clip
   let pos2 = ivec2(max(-pos.x, 0), max(-pos.y, 0))
   let pos = pos + pos2
   let size = ivec2(
-    size.x.min(g.width - pos2.x).min(r.width.int32 - pos.x),
-    size.y.min(g.height - pos2.y).min(r.height.int32 - pos.y)
+    size.x.min(g.width.int32 - pos2.x).min(r.width.int32 - pos.x),
+    size.y.min(g.height.int32 - pos2.y).min(r.height.int32 - pos.y)
   )
   if size.x <= 0 or size.y <= 0: return
 
@@ -35,6 +34,7 @@ func drawGlyph(r: Image, g: Glyph, pos: IVec2, size: IVec2) =
       inc idst
 
 proc vertical_line*(r: Image; x, y, h: int32, color: ColorRgb) =
+  ## draw vertical line (fast)
   let x = x.min(r.width).max(0)
   for i in countup(y.min(r.height).max(0) * r.width + x, (y + h).min(r.height).max(0) * r.width + x, r.width):
     r.data[i] = rgbx(color.r, color.g, color.b, 255)
@@ -43,6 +43,7 @@ proc vertical_line*(r: Image; x, y, h: int32, color: ColorRgb) =
 
 
 proc render(c: Rune, font: Font; fg, bg: ColorRgb): Image =
+  ## renter text to image
   let ts = font.typeset($c)
   let bounds = ts.computeBounds
   if bounds.x < 1 or bounds.y < 1: return nil
@@ -52,35 +53,40 @@ proc render(c: Rune, font: Font; fg, bg: ColorRgb): Image =
   result.fillText(ts)
 
 proc render(gt: var GlyphTable, c: Rune; fg, bg: ColorRgb) =
+  ## pre-rener text and save it in table
   let img = c.render(gt.font, fg, bg)
   if img == nil:
-    gt.glyphs[(c, fg, bg)] = Glyph.default
+    gt.glyphs[(c, fg, bg)] = nil
   else:
-    gt.glyphs[(c, fg, bg)] = (width: img.width.int32, height: img.height.int32, data: img.data)
+    gt.glyphs[(c, fg, bg)] = img
 
 func clear*(gt: var GlyphTable) =
   clear gt.glyphs
 
 
 proc draw*(r: Image, text: seq[ColoredText], box: Rect, gt: var GlyphTable, bg: ColorRgb) =
+  ## draw colored text
   var (x, y, w, h) = (box.x.round.int32, box.y.round.int32, box.w.round.int32, box.h.round.int32)
   for cs in text:
     let color = cs.color
     for c in cs.text.runes:
       let glyph =
-        try: gt.glyphs[(c, color, bg)].addr
+        try: gt.glyphs[(c, color, bg)]
         except KeyError:
           gt.render(c, color, bg)
-          gt.glyphs[(c, color, bg)].addr
+          gt.glyphs[(c, color, bg)]
+
       if not c.isWhiteSpace:
-        r.drawGlyph glyph[], ivec2(x, y), ivec2(w, h)
-      let bx = glyph[].width
+        r.draw glyph, ivec2(x, y), ivec2(w, h)
+
+      let bx = glyph.width.int32
       x += bx
       w -= bx
       if w <= 0: return
 
 
 proc width*(text: string, gt: var GlyphTable): int32 =
+  ## get width of text in pixels for font, specified in gt
   for c in text.runes:
     if (c, rgb(0, 0, 0), rgb(0, 0, 0)) notin gt.glyphs: gt.render(c, rgb(0, 0, 0), rgb(0, 0, 0))
-    result += gt.glyphs[(c, rgb(0, 0, 0), rgb(0, 0, 0))].width
+    result += gt.glyphs[(c, rgb(0, 0, 0), rgb(0, 0, 0))].width.int32
