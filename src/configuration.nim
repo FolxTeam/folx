7e7c2b15057a42928029a7b6b96b94281770441b
levovix0 <xlevovix@gmail.com>
2022-02-14 20:35:54 +0300

    refactor: configuration add: generating default theme file

--- src/configuration.nim
+++ src/configuration.nim
@@ -3,9 +3,13 @@ import jsony, pixie
 
 type
   Config* = object
-    font*: string
     colorTheme*: string
+
+    font*: string
     fontSize*: float32
+
+    interfaceFont*: string
+    interfaceFontSize*: float32
 
     window*: WindowConfig
 
@@ -19,6 +23,7 @@ type
     verticalline*: ColorRGB
     linenumbers*: ColorRGB
     textarea*: ColorRGB
+    statusBarBg*: ColorRGB
 
     sKeyword*: ColorRGB
     sOperatorWord*: ColorRGB
@@ -34,20 +39,54 @@ type
     sLineNumber*: ColorRGB
     sElse*: ColorRGB
 
+
 proc parseHook*[T](s: string, i: var int, v: var GVec2[T]) =
-  var a: array[2, T]
-  parseHook(s, i, a)
-  v.x = a[0]
-  v.y = a[1]
+  try:
+    var a: array[2, T]
+    parseHook(s, i, a)
+    v.x = a[0]
+    v.y = a[1]
+  except: discard
 
 proc dumpHook*[T](s: var string, v: GVec2[T]) =
   s.add [v.x, v.y].toJson
+
+
+proc parseHook*(s: string, i: var int, v: var ColorRGB) =
+  try:
+    var str: string
+    parseHook(s, i, str)
+    v = parseHex(str).rgb
+  except: discard
+
+proc dumpHook*(s: var string, v: ColorRGB) =
+  s.add v.color.toHex.toJson
+
+
+const defaultConfig = Config(
+  colorTheme: "resources/themes/dark.json",
+
+  font: "resources/FiraCode-Regular.ttf",
+  fontSize: 11'f32,
+
+  interfaceFont: "resources/Roboto-Regular.ttf",
+  interfaceFontSize: 13'f32,
+  
+  window: WindowConfig(
+    size: ivec2(1280, 720),
+  ),
+
+  file: "src/folx.nim",
+)
+static: writeFile "config.default.json", defaultConfig.toJson.parseJson.pretty
+
 
 const defaultColorTheme = ColorTheme(
   scrollbar: rgb(48, 48, 48),
   verticalline: rgb(64, 64, 64),
   linenumbers: rgb(32, 32, 32),
   textarea: rgb(32, 32, 32),
+  statusBarBg: rgb(0, 122, 204),
 
   sKeyword: rgb(86, 156, 214),
   sOperatorWord: rgb(86, 156, 214),
@@ -63,41 +102,24 @@ const defaultColorTheme = ColorTheme(
   sLineNumber: rgb(133, 133, 133),
   sElse: rgb(212, 212, 212),
 )
+static: writeFile "resources/themes/default.json", defaultColorTheme.toJson.parseJson.pretty
 
-const defaultConfig = Config(
-  font: "resources/FiraCode-Regular.ttf",
-  colorTheme: "resources/dark_theme.json",
-  fontSize: 11'f32,
-  
-  window: WindowConfig(
-    size: ivec2(1280, 720),
-  ),
-
-  file: "src/folx.nim",
-)
-static: writeFile "config.default.json", defaultConfig.toJson.parseJson.pretty
 
 proc newHook*(x: var Config) =
   x = defaultConfig
 
-proc parseHook*(s: string, i: var int, v: var ColorRGB) =
-  var str: string
-  parseHook(s, i, str)
-  v = parseHex(str).rgb
-
 proc newHook*(x: var ColorTheme) =
   x = defaultColorTheme
 
-proc readTheme*(file="resouces/dark_theme.json"): ColorTheme =
-
-  try:  file.readFile.fromJson(ColorTheme)
-  except:
-    # TODO: notification ui component
-    echo "Couldn't read the theme file"
-    return defaultColorTheme
 
 proc readConfig*(file="config.json"): Config =
   try:    file.readFile.fromJson(Config)
   except: defaultConfig
 
-let colorTheme* = readTheme(readConfig().colorTheme)
+proc readTheme*(file: string): ColorTheme =
+  try:    file.readFile.fromJson(ColorTheme)
+  except: defaultColorTheme
+
+
+let config* = readConfig()
+let colorTheme* = readTheme(config.colorTheme)
--- src/folx.nim
+++ src/folx.nim
@@ -38,24 +38,6 @@ proc indentation(text: seq[string]): Indentation =
     elif result[i].len.len > lgl: result[i].len = result[i].len[0..<lgl]
 
 
-proc status_bar(
-  r: Context,
-  image: Image,
-  box: Rect,
-  pos: float32,
-  gt: var GlyphTable,
-  bg: ColorRgb,
-  text: string,
-  ) =
-
-  var box = box
-
-  r.fillStyle = bg
-  r.fillRect box
-
-  var y = round(box.y - gt.font.size * 1.27 * (pos mod 1))
-  image.draw text, @[(sText.color, 0)], box, gt, bg
-  y += round(gt.font.size * 1.27)
 
 proc line_numbers(
   r: Context,
@@ -75,6 +57,7 @@ proc line_numbers(
     let w = float32 s.width(gt)
     image.draw s, @[(sLineNumber.color, 0)], rect(vec2(box.x + box.w - w, y), vec2(w, box.h - y)), gt, bg
     y += round(gt.font.size * 1.27)
+
 
 proc text_area(
   r: Context,
@@ -101,10 +84,11 @@ proc text_area(
 
     var x = box.x.round.int
     for i, l in indentation[i].len:
-      image.vertical_line x.int32, y.int32, dy.int32, configuration.colorTheme.verticalline
+      image.vertical_line x.int32, y.int32, dy.int32, colorTheme.verticaLline
       x += l * space_w
 
     y += dy
+
 
 proc scroll_bar(r: Context, box: Rect, pos: float32, size: int, total: int) =
   if total == 0: return
@@ -116,20 +100,41 @@ proc scroll_bar(r: Context, box: Rect, pos: float32, size: int, total: int) =
   box.xy = vec2(box.x, box.y + (box.h * a))
   box.wh = vec2(box.w, box.h * (b - a))
 
-  r.fillStyle = configuration.colorTheme.scrollbar
+  r.fillStyle = colorTheme.scrollbar
   r.fillRect box
 
-let config = readConfig()
+
+proc status_bar(
+  r: Context,
+  image: Image,
+  box: Rect,
+  pos: float32,
+  gt: var GlyphTable,
+  bg: ColorRgb,
+  text: string,
+  ) =
+  const margin = vec2(8, 2)
+
+  r.fillStyle = bg
+  r.fillRect box
+
+  image.draw text, @[(sText.color, 0)], rect(box.xy + margin, box.wh - margin), gt, bg
+
+
 
 let window = newWindow("folx", config.window.size, visible=false)
 
 window.title = config.file & " - folx"
 
-let font = readFont(config.font)
-font.size = config.fontSize
-font.paint.color = color(1, 1, 1, 1)
-var editor_gt = GlyphTable(font: font)
-var interface_gt = GlyphTable(font: font)
+let editor_font = readFont(config.font)
+editor_font.size = config.fontSize
+editor_font.paint.color = color(1, 1, 1, 1)
+var editor_gt = GlyphTable(font: editorFont)
+
+let interface_font = readFont(config.interfaceFont)
+interface_font.size = config.interfaceFontSize
+interface_font.paint.color = color(1, 1, 1, 1)
+var interface_gt = GlyphTable(font: interfaceFont)
 
 var text = config.file.readFile
 let text_indentation = text.splitLines.indentation
@@ -147,31 +152,32 @@ var
 
 proc animate(dt: float32): bool =
   if pos != visual_pos:
-    let pvp = (visual_pos * font.size).round.int32
+    let pvp = (visual_pos * editor_font.size).round.int32
 
     let d = (abs(pos - visual_pos) * (0.00001 / dt)).max(0.1).min(abs(pos - visual_pos))
     if pos > visual_pos: visual_pos += d
     else:                visual_pos -= d
 
-    if abs(visual_pos - pos) < 1 / font.size / 2.1:
+    if abs(visual_pos - pos) < 1 / editor_font.size / 2.1:
       visual_pos = pos
-    if pvp != (visual_pos * font.size).round.int32: result = true
+    if pvp != (visual_pos * editor_font.size).round.int32: result = true
+
 
 proc display =
   let
-    size = (window.size.y.float32 / font.size).ceil.int
+    size = (window.size.y.float32 / editor_font.size).ceil.int
     total = lines.len
 
     line_number_width = float32 ($total).width(editor_gt)
 
-  image.fill configuration.colorTheme.textarea
+  image.fill colorTheme.textarea
 
   r.line_numbers(
     image = image,
     box = rect(vec2(10, 0), vec2(line_number_width, window.size.vec2.y)),
     pos = visual_pos,
     gt = editor_gt,
-    bg = configuration.colorTheme.textarea,
+    bg = colorTheme.textarea,
     text = lines,
   )
   r.text_area(
@@ -179,7 +185,7 @@ proc display =
     box = rect(vec2(line_number_width + 30, 0), window.size.vec2 - vec2(10, 20)),
     pos = visual_pos,
     gt = editor_gt,
-    bg = configuration.colorTheme.textarea,
+    bg = colorTheme.textarea,
     text = lines,
     colors = colors,
     indentation = text_indentation,
@@ -195,7 +201,7 @@ proc display =
     box = rect(vec2(0, window.size.vec2.y - 20), vec2(window.size.vec2.x, 20)),
     pos = visual_pos,
     gt = interface_gt,
-    bg = configuration.colorTheme.sKeyword,
+    bg = colorTheme.statusBarBg,
     text = "visual_pos: " & $visual_pos,
   )
 
@@ -211,7 +217,7 @@ window.onCloseRequest = proc =
 window.onScroll = proc =
   if window.scrollDelta.y == 0: return
   if window.buttonDown[KeyLeftControl] or window.buttonDown[KeyRightControl]:
-    font.size = (font.size + window.scrollDelta.y.ceil).max(1)
+    editor_font.size = (editor_font.size + window.scrollDelta.y.ceil).max(1)
     clear editor_gt
     displayRequest = true
   else:
